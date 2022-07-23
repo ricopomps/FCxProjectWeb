5ed47d17c4eda41762fe7f7a35fced5361f7745f
ricopomps <ricardopompiliodossantos@gmail.com>
2022-07-23 20:47:03 -0300

    feat: Implemented scss on appbar

--- src/components/AppBar.tsx
+++ src/components/AppBar.tsx
@@ -1,21 +1,23 @@
 import { AppBar as AppBarMui, Toolbar, IconButton, Stack, Button, Link } from '@mui/material';
 import HomeIcon from '@mui/icons-material/Home';
+import '../styles/appBar.scss';
+
 type AppBarProps = {
 };
 export function AppBar (props:AppBarProps) {
   return (
     <AppBarMui position='static'>
-      <Toolbar>
+      <Toolbar >
         <Link href="/">
-            <IconButton style={{ color: '#fafafa' }} size='large' edge="start" aria-label="logo">
+            <IconButton className="link" size='large' edge="start" aria-label="logo">
                 <HomeIcon/>
             </IconButton>
         </Link>
         <Stack direction='row' spacing={2}>
-            <Link style={{ color: '#fafafa' }} href="/list">
+            <Link className="link" href="/list">
                 <Button color='inherit'>Listagem</Button>
             </Link>
-            <Link style={{ color: '#fafafa' }} href="/form">
+            <Link className="link" href="/form">
                 <Button color='inherit'>Criação</Button>
             </Link>
         </Stack>
